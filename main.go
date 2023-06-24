dd52f06461bca6207732df27c9fdad7462eaec6a
jag6 <schecter1771@hotmail.com>
2023-06-24 23:33:45 +0900

    init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>hiddey-ho gopherino</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>Contact</h1>
+		<p>to leave a message, drop me an email at <a href=\"\">this</a></p>
+	`)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>FAQ Page</h1>
+		<ul>
+			<li>
+				<b>Is there a free version?</b>
+				Yes! We offer a free trial for 30 days on any paid plans.
+			</li>
+			<li>
+				<b>What are your support hours?</b>
+				We have support staff answering emails 24/7, though response
+				times may be a bit slower on weekends.
+			</li>
+			<li>
+				<b>How do I contact support?</b>
+				Email us - <a href="mailto:support@fotogen.com">support@fotogen.com</a>
+			</li>
+		</ul>
+	`)
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", 404)
+	})
+
+	fmt.Println("starting server on :3000")
+	http.ListenAndServe(":3000", r)
+}
